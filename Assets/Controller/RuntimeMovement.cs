b1d7c30a2b45628b39f3f1adff41fc1ffac7e050
Meryem Merve YILDIZ <mmyildiz97@gmail.com>
2022-05-09 23:57:56 +0300

    Added Documents

---
+++ Assets/Controller/RuntimeMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class RuntimeMovement : MonoBehaviour
+{
+    private Movement _input;
+    private CharacterController _controller;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _input = GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _controller.Move(new Vector3(x: _input.moveVal.x * _input.moveSpeed, y: 0f, z: _input.moveVal.y * _input.moveSpeed));
+    }
+
+
+}
